a5e1828647393689b6a4b19120b7425adda3ba81
Irihosejeandedieu <irhosejd2023@gmail.com>
2023-12-16 15:33:43 +0300

    4-puts.c and 3-name

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,18 +1,15 @@
-#include <stio.h>
+#include <stdio.h>
 
 /**
- * main - Entry point
- *
- * a C program that prints exactly "Programming is like building a multilingual puzzle, 
- * followed by a new line.
- *
+ * main - prints "programming is like building a multilingual 
+ * puzzle,followed by newline.
  * Return: Always 0.
  */
 
-int main (void)
 
-{
-	puts("\"Programming is like building a multilingual puzzle");
+int main(void)
+
+{      puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
 
