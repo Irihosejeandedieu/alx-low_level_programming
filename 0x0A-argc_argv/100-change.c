b7ebb61595785100e20c4e0db607a32fae640d57
Irihosejeandedieu <irhosejd2023@gmail.com>
2024-01-12 23:05:41 +0300

    correctionn..

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -15,25 +15,24 @@ int coinConverter(int i)
 
 	while (i != 0)
 	{
-		if (i % 10 == 9 || i % 10 == 7)
-			i -= 2;
-		else if (i % 25 == 0)
-			i -= 25;
-		else if (i % 10 == 0)
-			i -= 10;
-		else if (i % 5 == 0)
-			i -= 5;
-		else if (i % 2 == 0)
-		{
-			if (i % 10 == 6)
-				i -= 1;
-			else
-				i -= 2;
-		}
-		else
-			i -= 1;
-
-		count++;
+	if (i % 10 == 9 || i % 10 == 7)
+	i -= 2;
+	else if (i % 25 == 0)
+	i -= 25;
+	else if (i % 10 == 0)
+	i -= 10;
+	else if (i % 5 == 0)
+	i -= 5;
+	else if (i % 2 == 0)
+	{
+	if (i % 10 == 6)
+	i -= 1;
+	else
+	i -= 2;
+	}
+	else
+	i -= 1;
+	count++;
 	}
 
 	return (count);
@@ -53,19 +52,19 @@ int main(int argc, char *argv[])
 
 	if (argc != 2)
 	{
-		printf("Error\n");
-		return (1);
+	printf("Error\n");
+	return (1);
 	}
 
 	i = atoi(argv[1]);
 
 	if (i < 0)
-		printf("0\n");
+	printf("0\n");
 	else
 	{
-		coin = coinConverter(i);
+	coin = coinConverter(i);
 
-		printf("%d\n", coin);
+	printf("%d\n", coin);
 	}
 
 	return (0);
