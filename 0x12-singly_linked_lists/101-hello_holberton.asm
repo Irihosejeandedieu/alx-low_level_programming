336d17bf0543617b20b0e91bf978a069e788e598
Irihosejeandedieu <irhosejd2023@gmail.com>
2024-02-01 04:31:34 +0300

    lastly

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,1 +1,15 @@
+section .data
+    hello db 'Hello, Holberton', 0xA ; 0xA is the newline character
 
+section .text
+    extern printf
+
+global main
+main:
+    ; Call printf
+    mov rdi, hello
+    call printf
+
+    ; Return 0 from main
+    mov eax, 0
+    ret
