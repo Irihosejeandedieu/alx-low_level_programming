dacbdefb309c34be71ff3c696455609d4723e78b
irihose jean de dieu <irhosejd2023@gmail.com>
2024-03-26 00:19:40 -0700

    add&sub

---
+++ 0x18-dynamic_libraries/add.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+
+/**
+ * add - add two integers
+ * @a :the first value
+ * @b:the second integer
+ * Return:addition
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
---
+++ 0x18-dynamic_libraries/sub.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+
+/**
+ * subtract - subtract two integers
+ * @a :the first value
+ * @b:the second integer
+ * Return:subtraction
+ */
+
+int sub(int a, int b)
+{
+        return (a - b);
